93e6f1b352537c3ecafac3c37f9ea860464f5f0a
buathi_q <buathi_q@epitech.eu>
2014-12-04 14:07:21 +0100

    Basic deploying script

--- config/index.js
+++ config/index.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var dotenv = require('dotenv');
+dotenv.load();
+
 module.exports = {
   password: process.env.PASSWORD || 'anyfetch',
   opsUrl: process.env.OPS_URL || 'http://anyfetch-ops.herokuapp.com'
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,20 +7,25 @@ var config = require('../config');
 function deploy(msg, apps, env) {
   request(config.opsUrl)
     .post('/deploy')
-    .send('env=' + env + '&app=' + apps.join(' ') + '&user=' + msg.envelope.user.name + '&password=' + config.password)
+    .send({
+      end: env,
+      app: apps.join(' '),
+      user: msg.envelope.user.name,
+      password: config.password
+    })
     .end(function(err, res) {
-      if(err || res.text !== 'Deploying') {
-        msg.send(err ? err.toString() : res.text);
+      if(err || res.body !== 'Deploying') {
+        msg.send(err ? err.toString() : res.body);
       }
 
-      msg.send("Deploying " + apps.join(', ') + ' to ' + env);
+      msg.send("Deploying " + apps.join(', ') + ' on ' + env);
     });
 }
 
 module.exports = function initDeploy(robot) {
-  robot.respond(/deploy ([a-z0-9, ]+)( to (staging|production))?/i, function(msg) {
+  robot.respond(/deploy (([^,]*)( ?, ?([^,]*))*)( on (staging|production))/i, function(msg) {
     var apps = msg.match[1].split(/,| /);
-    var env = msg.match[3] || 'staging';
+    var env = msg.match[6] || 'staging';
 
     for(var i = 0, c = apps.length; i < c; i += 1) {
       if(!apps[i]) {
