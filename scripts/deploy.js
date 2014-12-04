bc7b1379b869d6cd88eef29b2c370fad6a791dff
buathi_q <buathi_q@epitech.eu>
2014-12-04 17:22:49 +0100

    Better regexp for deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -23,9 +23,9 @@ function deploy(msg, apps, env) {
 }
 
 module.exports = function initDeploy(robot) {
-  robot.respond(/deploy (([^,]*)( ?, ?([^,]*))*)( on (staging|production))?/i, function(msg) {
+  robot.respond(/deploy (.+?)(?: (?:on (staging|production))?$|$)/i, function(msg) {
     var apps = msg.match[1].split(/,| /);
-    var env = msg.match[6] || 'staging';
+    var env = msg.match[2] || 'staging';
 
     for(var i = 0, c = apps.length; i < c; i += 1) {
       if(!apps[i]) {
